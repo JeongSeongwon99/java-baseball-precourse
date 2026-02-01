c8a2119ce4f38a0b7c5b0bdae576ab88b88962e3
frank.jeong <frank.jeong@frankjeongui-MacBookPro.local>
2026-02-01 23:28:59 +0900

    feat(game): add playing game service (add GameService)

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -1,16 +1,33 @@
 package service;
 
+import domain.JudgeCount;
 import domain.Numbers;
 
 public class GameService {
     private final RandomNumberGenerator randomNumberGenerator;
-    private Numbers answer;
+    private final JudgeService judgeService;
 
-    public GameService(RandomNumberGenerator randomNumberGenerator) {
+    private Numbers answer;
+    private JudgeCount lastCount;
+
+    public GameService(RandomNumberGenerator randomNumberGenerator, JudgeService judgeService) {
         this.randomNumberGenerator = randomNumberGenerator;
+        this.judgeService = judgeService;
     }
 
     public void startNewGame() {
         answer = randomNumberGenerator.numberGenerate();
+        lastCount = null;
+    }
+
+    public JudgeCount play(String guessInput) {
+        JudgeCount count = judgeService.judge(answer, Numbers.from(guessInput));
+        lastCount = count;
+        return count;
+    }
+
+    public boolean isGameOver() {
+        if (lastCount == null) return false;
+        return lastCount.isThreeStrikes();
     }
 }
