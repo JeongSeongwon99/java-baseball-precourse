327bac102ad20d711c5d80210c4eac0760f373e4
frank.jeong <frank.jeong@frankjeongui-MacBookPro.local>
2026-02-01 22:20:27 +0900

    feat(game) add inputValidator - check input

---
+++ src/main/java/util/InputValidator.java
@@ -0,0 +1,46 @@
+package util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class InputValidator {
+    private static final String ERROR_PREFIX = "[ERROR] ";
+    private static final int INPUT_LENGTH = 3;
+    private static final String RESTART_CODE = "1";
+    private static final String EXIT_CODE = "2";
+
+    public static String guessError(String input) {
+        if (input.length() != INPUT_LENGTH) {
+            return errorMessage(INPUT_LENGTH + "자여야 합니다.");
+        }
+        return validateCharacters(input);
+    }
+
+    private static String validateCharacters(String input) {
+        Set<Character> uniqueChars = new HashSet<>();
+
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return errorMessage("숫자만 입력해야 합니다.");
+            }
+            if (c == '0') {
+                return errorMessage("0은 사용할 수 없습니다.");
+            }
+            if (!uniqueChars.add(c)) {
+                return errorMessage("중복된 숫자는 사용할 수 없습니다.");
+            }
+        }
+        return null;
+    }
+
+    public static String restartError(String input) {
+        if (RESTART_CODE.equals(input) || EXIT_CODE.equals(input)) {
+            return null;
+        }
+        return errorMessage(String.format("재시작은 %s, 종료는 %s만 입력 가능합니다.", RESTART_CODE, EXIT_CODE));
+    }
+
+    private static String errorMessage(String message) {
+        return ERROR_PREFIX + message;
+    }
+}
