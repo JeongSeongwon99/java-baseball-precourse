b18ee638c5faf195cbf2b3de73f60d26fd6326ae
frank.jeong <frank.jeong@frankjeongui-MacBookPro.local>
2026-02-02 21:48:11 +0900

    feat(game): 스트라이크 볼 순서 바꾸기

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -28,11 +28,11 @@ public class OutputView {
         if (ball == 0 && strike == 0) return "낫싱";
 
         StringBuilder sb = new StringBuilder();
-        if (ball > 0) {
-            sb.append(ball).append("볼 ");
-        }
         if (strike > 0) {
-            sb.append(strike).append("스트라이크");
+            sb.append(strike).append("스트라이크 ");
+        }
+        if (ball > 0) {
+            sb.append(ball).append("볼");
         }
         return sb.toString().trim();
     }
